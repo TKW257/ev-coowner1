bb8c6fbf7032db653fc868ffbee56dca46b60c08
phutran955 <trangiaphu54@gmail.com>
2025-10-20 13:26:21 +0700

    up

--- frontend/src/components/sidebar/index.jsx
+++ frontend/src/components/sidebar/index.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { Menu } from "antd";
-import { HomeOutlined, CalendarOutlined, BookOutlined, FileTextOutlined, } from "@ant-design/icons";
+import {
+  HomeOutlined,
+  CalendarOutlined,
+  BookOutlined,
+  DashboardOutlined,
+  FileTextOutlined,
+  CheckCircleOutlined,
+  LikeOutlined,
+} from "@ant-design/icons";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import logoFull from "../../assets/logo_main.png";
@@ -10,7 +18,7 @@ const Sidebar = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const currentUser = useSelector((state) => state.user.current);
-  const role = currentUser.role || "USER";
+  const role = currentUser?.role || "USER";
 
   let menuItems = [];
 
@@ -19,37 +27,46 @@ const Sidebar = () => {
       {
         key: "/admin/bookingmanage",
         icon: <BookOutlined />,
-        label: <Link to="/admin/bookingmanage"> Bookings</Link>,
+        label: <Link to="/admin/bookingmanage">Bookings</Link>,
       },
       {
-        key: "invoicemanagement",
+        key: "/admin/staffchecking",
+        icon: <CheckCircleOutlined />,
+        label: <Link to="/admin/staffchecking">Staff Checking</Link>,
+      },
+      {
+        key: "/admin/vote",
+        icon: <LikeOutlined />,
+        label: <Link to="/admin/vote">Vote Management</Link>,
+      },
+      {
+        key: "/admin/invoice",
         icon: <BookOutlined />,
-        label: <Link to="/admin/invoice"> Hóa đơn</Link>,
+        label: <Link to="/admin/invoice">Hóa đơn</Link>,
       },
     ];
   } else if (role === "USER") {
     menuItems = [
       {
-        key: "mycar",
+        key: "/owner/mycar",
         icon: <HomeOutlined />,
-        label: <Link to="/owner/mycar">Xe của tôi </Link>,
+        label: <Link to="/owner/mycar">Xe của tôi</Link>,
       },
       {
-        key: "booking",
+        key: "/owner/carbooking",
         icon: <CalendarOutlined />,
-        label: <Link to="/owner/carbooking" >Đặt xe</Link>,
+        label: <Link to="/owner/carbooking">Đặt xe</Link>,
       },
-        {
-        key: "invoice",
+      {
+        key: "/owner/invoice",
         icon: <FileTextOutlined />,
-        label: <Link to="/owner/invoice" >Hóa đơn</Link>,
+        label: <Link to="/owner/invoice">Hóa đơn</Link>,
       },
     ];
   }
 
   return (
     <div className="owner-sidebar">
-
       <div className="sidebar-logo" onClick={() => navigate("/")}>
         <img src={logoFull} alt="CoEV Logo" className="logo-img" />
       </div>
