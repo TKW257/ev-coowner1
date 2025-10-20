0233573373bf5b25fb9d877b996a7f23bd4eff24
Thang <tranhuuthangdt2572005@gmail.com>
2025-10-20 15:11:22 +0700

    update voteApi.js

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -5,18 +5,18 @@ const axiosClient = axios.create({
   baseURL: "https://vallate-enzootically-sterling.ngrok-free.dev/api",
   headers: {
     "Content-Type": "application/json",
-    "ngrok-skip-browser-warning": "true"
+    "ngrok-skip-browser-warning": "true",
   },
 });
 
 // Request Interceptor: tự động thêm token
 axiosClient.interceptors.request.use(async (config) => {
   const token = localStorage.getItem(StorageKeys.TOKEN);
-  
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-  
+
   return config;
 });
 
