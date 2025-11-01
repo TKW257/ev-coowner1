b1c141f33353abea92aef4f4ee98995c8fbe20c2
kietnase173510 <kietnase173510@fpt.edu.vn>
2025-11-01 09:50:12 +0700

    11/1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["caleb-idiomatic-milissa.ngrok-free.dev"],
+    allowedHosts: ["unentangleable-overinstructively-elle.ngrok-free.dev"],
   },
 });
